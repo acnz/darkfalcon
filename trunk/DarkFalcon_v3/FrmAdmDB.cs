887e8dbba1df3ec6d5cb25879ed7dc708e0a1db6
andre_cnz <andre_cnz@localhost>
2011-03-26 15:20:28 -0300

    teste de bd

---
+++ trunk/DarkFalcon_v3/FrmAdmDB.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DarkFalcon_v3
+{
+    public partial class FrmAdmDB : Form
+    {
+        public FrmAdmDB()
+        {
+            InitializeComponent();
+        }
+    }
+}
